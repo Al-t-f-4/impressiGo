2f14c38aaf8018fbb77fb9b69b4673f6162eb358
Alt f4 <ffvrgb@bk.ru>
2024-03-18 02:25:05 +0300

    I can eat glass and it doesn't hurt me

---
+++ example/hello.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+import "rsc.io/quote"
+
+func main() {
+	fmt.Println("Hello, world!")
+    fmt.Println(quote.Go())
+    fmt.Println(quote.Glass())
+    fmt.Println(quote.Hello())
+    fmt.Println(quote.Opt())
+}
+
+
