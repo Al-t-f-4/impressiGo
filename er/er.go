6a2aa8e6d2c7fdf38ba1a22470ac318a7000c299
Alt f4 <ffvrgb@bk.ru>
2024-03-30 00:30:54 +0300

    использовал рандомайзер в связке с массивом строк

--- er/er.go
+++ er/er.go
@@ -3,6 +3,7 @@ package er
 import ( 
     "fmt"
     "errors"
+    "math/rand"
 )
 
 func My_err ( name string ) ( string, error ) {
@@ -10,7 +11,17 @@ func My_err ( name string ) ( string, error ) {
         return "", errors.New ( "empty name" )
     }
 
-    msg := fmt.Sprintf ( "Привет, мое имя - %v.", name )
+    msg := fmt.Sprintf ( randomFormat(), name )
 
     return msg, nil
+}
+
+func randomFormat() string {
+    formats := []string{
+        "Привет, %v. Добро пожаловать!",
+        "Рад тебя видеть, %v!",
+        "HAIL, %v! СААААлаааам!",
+    }
+
+    return formats[rand.Intn(len(formats))]
 }
